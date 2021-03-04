05fc5b1aa18765dcdc38e4e20451408a5b1dc8eb
Laurence Pakenham-Smith <laurence.smith@airelogic.com>
2021-03-04 12:09:15 +0000

    APM-1662 Replace sandbox with something simpler

---
+++ sandbox/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', defaults={"path": ""})
+@app.route('/<path:path>')
+def hello_world(path):
+    return f"Hello world! Path: {path}"
