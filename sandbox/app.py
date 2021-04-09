abfa09a183489d28b0be69bcf3abf80ec1831dbf
Ryszard Madejski <r.madejski@kainos.com>
2021-04-09 14:08:55 +0200

    Verifying build and deploy works, and access to all necessary resources

--- sandbox/app.py
+++ sandbox/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/', defaults={"path": ""})
 @app.route('/<path:path>')
 def hello_world(path):
-    return f"Hello world! Path: {path}"
+    return f"Hello world test! Path: {path}"
