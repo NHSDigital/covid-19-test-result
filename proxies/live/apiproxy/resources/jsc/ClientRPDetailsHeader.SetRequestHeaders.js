be19531b3f3b53e3ae42e713acffe9d827b9cf02
Ryszard Madejski <r.madejski@kainos.com>
2021-06-14 08:06:55 +0200

    HTC-3558: add proofing level header

--- proxies/live/apiproxy/resources/jsc/ClientRPDetailsHeader.SetRequestHeaders.js
+++ proxies/live/apiproxy/resources/jsc/ClientRPDetailsHeader.SetRequestHeaders.js
@@ -1,10 +1,12 @@
 var developerAppName = context.getVariable('developer.app.name');
 var developerAppId = context.getVariable('developer.app.id');
 var clientIP = context.getVariable('client.ip');
+var proofingLevel = context.getVariable('developer.app.allowed-proofing-level');
 
 var clientRpDetailsHeader = {
     "developer.app.name": developerAppName,
     "developer.app.id": developerAppId,
+    "developer.app.allowed-proofing-level": proofingLevel,
     "client.ip": clientIP
 };
 
